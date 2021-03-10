d49aa38c3abd99fbbc3f8c177b71cc35acae851c
Michal Januszko <michaljanuszko@gmail.com>
2021-03-10 07:48:04 +0100

    Added routing with react-router-dom

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,5 +1,6 @@
 import { observer } from 'mobx-react-lite';
 import React, { SyntheticEvent, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { Button, Item, Label, Segment } from 'semantic-ui-react';
 import { useStore } from '../../../app/stores/store';
 
@@ -7,7 +8,6 @@ export default observer(function ActivityList() {
   const { activityStore } = useStore();
   const {
     deleteActivity,
-    selectActivity,
     activitiesByDate: activities,
     loading,
   } = activityStore;
@@ -38,7 +38,8 @@ export default observer(function ActivityList() {
               </Item.Description>
               <Item.Extra>
                 <Button
-                  onClick={() => selectActivity(activity.id)}
+                  as={Link}
+                  to={`/activities/${activity.id}`}
                   floated='right'
                   content='View'
                   color='blue'
