9f77bf8cde0053d8066531bc078e75e8332b47a8
Michał Januszko <michaljanuszko@gmail.com>
2021-05-03 16:42:04 +0200

    Added attendee popup.

---
+++ client-app/src/features/profiles/ProfileCard.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Profile} from "../../app/models/profile";
+import {observer} from "mobx-react-lite";
+import {Card, Icon, Image} from "semantic-ui-react";
+import {Link} from "react-router-dom";
+
+interface Props {
+    profile: Profile;
+}
+
+const ProfileCard = ({profile}: Props) => {
+    return (
+        <Card as={Link} to={`/profiles/${profile.username}`}>
+            <Image src={profile.image || '/assets/user.png'} />
+            <Card.Content>
+                <Card.Header>{profile.displayName}</Card.Header>
+                <Card.Description>Bio...</Card.Description>
+                <Card.Content extra><Icon name='user' />20 followers</Card.Content>
+            </Card.Content>
+        </Card>
+    );
+};
+
+export default observer(ProfileCard);
